3bc1d24577b5fa1693f5ca4d744e937155be7cba
MhmdSairi© <40028734+MhmdSairi@users.noreply.github.com>
2026-06-26 16:49:01 +0800

    Add files via upload

---
+++ src/components/ui/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+import { DM_Sans, Bebas_Neue } from "next/font/google";
+import "./globals.css";
+import { Toaster } from "@/components/ui/sonner";
+import QueryProvider from "@/components/QueryProvider";
+
+const dmsans = DM_Sans({
+  variable: "--font-dm-sans",
+  subsets: ["latin"],
+});
+
+const bebasNeue = Bebas_Neue({
+  weight: "400",
+  variable: "--font-bebas-neue",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "NIKAHFIX | wedding of Sairi & Diana",
+  description: "the wedding of Sairi & Diana",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang='en'>
+      <body className={`${dmsans.variable} ${bebasNeue.variable} antialiased`}>
+        <QueryProvider>{children}</QueryProvider>
+        <Toaster />
+      </body>
+    </html>
+  );
+}
